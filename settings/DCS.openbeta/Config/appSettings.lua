b62d386e8e56e5c6b6cf2ac2ae29da0b0b8a1a97
Andykan <andishe.kanani@gmail.com>
2026-02-17 19:52:10 +0100

    screen export

--- settings/DCS.openbeta/Config/appSettings.lua
+++ settings/DCS.openbeta/Config/appSettings.lua
@@ -1,39 +1,6 @@
 windowPlacement = {
-    ["y"] = 45,
-    ["x"] = -4094,
-    ["w"] = 4096,
-    ["h"] = 2160,
-}
-
-monitors = {
-    [1] = {
-        ["rcMonitor"] = {
-            ["top"] = 0,
-            ["right"] = 0,
-            ["left"] = -4096,
-            ["bottom"] = 2160,
-        },
-        ["dwFlags"] = 0,
-        ["rcWork"] = {
-            ["top"] = 0,
-            ["right"] = 0,
-            ["left"] = -4096,
-            ["bottom"] = 2160,
-        },
-    },
-    [2] = {
-        ["rcMonitor"] = {
-            ["top"] = 0,
-            ["right"] = 1440,
-            ["left"] = 0,
-            ["bottom"] = 2560,
-        },
-        ["dwFlags"] = 1,
-        ["rcWork"] = {
-            ["top"] = 0,
-            ["right"] = 1440,
-            ["left"] = 0,
-            ["bottom"] = 2560,
-        },
-    },
+	["y"] = 0,
+	["x"] = 0,
+	["w"] = 4096,
+	["h"] = 3184,
 }
